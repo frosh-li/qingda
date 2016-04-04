adc3b4a587dd660e85b61c898eef0f1df5e39e7c
flavorzhu <haker-haker@163.com>
2016-04-04 17:19:03 +0800

    delete backup file

--- api/protected/config/roles.php
+++ api/protected/config/roles.php
@@ -4,4 +4,3 @@ defined('BACKEND_ADMIN') or define('BACKEND_ADMIN','0');
 defined('ROLE_ADMIN') or define('ROLE_ADMIN','1');
 defined('ROLE_CUSTOMADMIN') or define('ROLE_CUSTOMADMIN','2');
 defined('ROLE_INFOADMIN') or define('ROLE_INFOADMIN','3');
-?>
