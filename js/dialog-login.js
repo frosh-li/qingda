b2a5f0f686700a618689f39bcb182771c358e177
lijunliang <junguoliang@163.com>
2016-11-03 19:04:10 +0800

    change account redirect to home page

--- js/dialog-login.js
+++ js/dialog-login.js
@@ -15,7 +15,9 @@ define(['require','api','common'],function(require,API,common){
                     _this.listenTo(Backbone.Events,"login:box",function(data){
                         console.log(data);
                         localStorage.setItem('userinfo', JSON.stringify(data));
+                        window.location.href="#/manage/station";
                         window.location.reload();
+                        _this.oncancel();
                     })
                     _this.listenTo(Backbone.Events,"login:box:fail",function(data){
                         alert('登录失败，请重试');
