97bd2acc258f0b69d13f270099db9e1a2c861818
pkgit2727 <272717894@qq.com>
2018-06-10 21:46:29 +0800

    根据人员隶属区域区分报警数据

--- api/protected/controllers/SystemalarmController.php
+++ api/protected/controllers/SystemalarmController.php
@@ -21,24 +21,50 @@ class SystemalarmController extends Controller
 	 */
 	public function actionIndex()
 	{
+        //需要根据my_sysuser的area区域字段来区分数据报警
+        //mysite的aid可以关联tree的id
+        $areas = "select area from my_sysuser where id = ".$_SESSION["uid"];
+        $auths = Yii::app()->db->createCommand($areas)->queryScalar();
+        // echo $auths;
+        $sn_key_list = array();
+        if ($auths != "*"){
+            $sql = "select serial_number from my_site where aid in ($auths)";
+            $search = Yii::app()->db->createCommand($sql)->queryAll();
+            foreach ($search as $value) {
+                $sn_key_list[] = $value['serial_number'];
+            }
+        }
+        unset($where,$sql);
+
+        $query = '';
+        if (count($sn_key_list) > 0){
+            $query = 'where systemalarm.station in ('.implode(',', $sn_key_list).')';
+        }
+
         $this->setPageCount();
-        $offset = ($this->page-1)*$this->count;
+        // $offset = ($this->page-1)*$this->count;
 
-        $sql = "select * from systemalarm  left join my_site on (my_site.serial_number=systemalarm.station) order by systemalarm.id desc";
-        $ups = Yii::app()->db->createCommand()
-                ->select("*")
-                ->from("systemalarm")
-                ->leftJoin("my_site","my_site.serial_number=systemalarm.station")
-                ->order("systemalarm.id desc")
-                ->limit($this->count)
-                ->offset(($this->page - 1) * $this->count)
-                ->queryAll();
+        // $sql = "select * from systemalarm  left join my_site on (my_site.serial_number=systemalarm.station) order by systemalarm.id desc";
+        // $ups = Yii::app()->db->createCommand()
+        //         ->select("*")
+        //         ->from("systemalarm")
+        //         ->leftJoin("my_site","my_site.serial_number=systemalarm.station")
+        //         ->order("systemalarm.id desc")
+        //         ->limit($this->count)
+        //         ->offset(($this->page - 1) * $this->count)
+        //         ->queryAll();
+        $end = $this->count;
+        $start = ($this->page - 1) * $end;
+        $sql = "select * from systemalarm left join my_site on my_site.serial_number = systemalarm.station $query order by systemalarm.id desc limit $start,$end";
+        $ups = Yii::app()->db->createCommand($sql)->queryAll();
 
-        $upsTotal = Yii::app()->db->createCommand()
-                ->select("count(*) as totals")
-                ->from("systemalarm")
-                ->order("systemalarm.id desc")
-                ->queryScalar();
+        // $upsTotal = Yii::app()->db->createCommand()
+        //         ->select("count(*) as totals")
+        //         ->from("systemalarm")
+        //         ->order("systemalarm.id desc")
+        //         ->queryScalar();
+        $sql = "select count(*) as totals from systemalarm $query order by systemalarm.id desc";
+        $upsTotal = Yii::app()->db->createCommand($sql)->queryScalar();
         
         $ret['response'] = array(
             'code' => 0,
