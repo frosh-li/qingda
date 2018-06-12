0fabd499ec6f0dd0ffaa62fd2c02bc49088ac543
root <upiaoaliyun>
2018-06-12 15:54:14 +0800

    修改php excel autoload bug

--- api/protected/extensions/PHPExcel/PHPExcel/Autoloader.php
+++ api/protected/extensions/PHPExcel/PHPExcel/Autoloader.php
@@ -24,7 +24,7 @@
  * @license    http://www.gnu.org/licenses/old-licenses/lgpl-2.1.txt    LGPL
  * @version    1.8.0, 2014-03-02
  */
-
+spl_autoload_unregister(array('YiiBase','autoload')); 
 PHPExcel_Autoloader::Register();
 //    As we always try to run the autoloader before anything else, we can use it to do a few
 //        simple checks and initialisations
@@ -34,7 +34,7 @@ if (ini_get('mbstring.func_overload') & 2) {
     throw new PHPExcel_Exception('Multibyte function overloading in PHP must be disabled for string functions (2).');
 }
 PHPExcel_Shared_String::buildCharacterSets();
-
+spl_autoload_register(array('YiiBase','autoload')); 
 
 /**
  * PHPExcel_Autoloader
